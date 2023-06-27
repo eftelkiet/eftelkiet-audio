4f8b7819a41e21ba144d79f90b2db0f1d740fccf
eftelkiet <eftelkiet@gmail.com>
2023-06-27 12:33:52 +0200

    Remove everything

--- src/client/services/socket/handlers/voice/HandleVoicePeerMovement.js
+++
@@ -1,8 +0,0 @@
-import {VoiceModule} from "../../../voice/VoiceModule";
-
-export function HandleVoicePeerMovement(data) {
-    for (let i = 0; i < data.updateSet.length; i++) {
-        let update = data.updateSet[i];
-        VoiceModule.peerLocationUpdate(update.streamKey, update.x, update.y, update.z);
-    }
-}
--- src/client/services/voice/errors/ConnectionClosedError.js
+++
@@ -1,6 +0,0 @@
-export class ConnectionClosedError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'ConnectionClosedError';
-    }
-}
